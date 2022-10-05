ce7a12a7b191e6d6e33796d91be7de10e0714f96
Chris Dryden <cdryden@imperosoftware.com>
2022-10-05 09:16:42 +0100

    Uses latest syntaxt for react dom rendering

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './src/app.js'
 
 const app = document.getElementById('app')
-ReactDOM.render(<App />, app)
+const root = createRoot(app)
+root.render(<App />)
